4b4501f88a014b5821b4e70ced0572d74526303e
Andcool-Systems <drolll2007@gmail.com>
2024-12-03 22:47:51 +0300

    added icon - name gap

--- components/MonitorDetail.tsx
+++ components/MonitorDetail.tsx
@@ -45,7 +45,11 @@ export default function MonitorDetail({
     const monitorNameElement = (
         <Text mt="sm" fw={700} style={{ display: 'inline-flex', alignItems: 'center' }}>
             {monitor.statusPageLink ? (
-                <a href={monitor.statusPageLink} target="_blank" style={{ display: 'inline-flex', alignItems: 'center', color: 'inherit' }}>
+                <a
+                    href={monitor.statusPageLink}
+                    target="_blank"
+                    style={{ display: 'inline-flex', alignItems: 'center', color: 'inherit', gap: '5px' }}
+                >
                     {statusIcon} {monitor.name}
                 </a>
             ) : (
