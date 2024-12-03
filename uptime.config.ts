7f817f3f8d036564c9e81e3be091bdf412074464
Andcool-Systems <drolll2007@gmail.com>
2024-12-03 15:09:08 +0300

    added projects

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,6 +1,6 @@
 const pageConfig = {
   // Title for your status page
-  title: "Andcool Services Uptime",
+  title: "Andcool Services Status",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://andcool.ru', label: 'Website' },
@@ -34,6 +34,123 @@ const workerConfig = {
       // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
       timeout: 10000,
       // [OPTIONAL] headers to be sent
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'personal_new',
+      name: 'New Personal Site',
+      method: 'GET',
+      target: 'https://new.andcool.ru',
+      tooltip: 'My new personal website',
+      statusPageLink: 'https://new.andcool.ru',
+      expectedCodes: [200],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'fu',
+      name: 'File Uploader Site',
+      method: 'GET',
+      target: 'https://fu.andcool.ru',
+      tooltip: 'File Uploader',
+      statusPageLink: 'https://fu.andcool.ru',
+      expectedCodes: [200],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'fu_api',
+      name: 'File Uploader API',
+      method: 'GET',
+      target: 'https://fu.andcool.ru/api',
+      tooltip: 'File Uploader API',
+      statusPageLink: 'https://fu.andcool.ru/api',
+      expectedCodes: [200],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'pplbandage',
+      name: 'PPLBandage Site',
+      method: 'GET',
+      target: 'https://pplbandage.ru',
+      tooltip: 'PPLBandage Site',
+      statusPageLink: 'https://pplbandage.ru',
+      expectedCodes: [200],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'pplbandage_api',
+      name: 'PPLBandage API',
+      method: 'GET',
+      target: 'https://pplbandage.ru/api/v1/ping',
+      tooltip: 'PPLBandage API',
+      statusPageLink: 'https://pplbandage.ru/api/v1',
+      expectedCodes: [200],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'weather',
+      name: 'Weather Widget Site',
+      method: 'GET',
+      target: 'https://weather.andcool.ru',
+      tooltip: 'Weather Widget Site',
+      statusPageLink: 'https://weather.andcool.ru',
+      expectedCodes: [200],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'weather_api',
+      name: 'Weather Widget API',
+      method: 'GET',
+      target: 'https://weather.andcool.ru/api',
+      tooltip: 'Weather Widget API',
+      statusPageLink: 'https://weather.andcool.ru/api',
+      expectedCodes: [200, 400],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'activity',
+      name: 'Andcool Activity API',
+      method: 'GET',
+      target: 'https://activity.andcool.ru',
+      tooltip: 'Andcool Activity API',
+      statusPageLink: 'https://activity.andcool.ru',
+      expectedCodes: [200, 404],
+      timeout: 10000,
+      headers: {
+        'User-Agent': 'Uptimeflare'
+      },
+    },
+    {
+      id: 'mc-oauth-api',
+      name: 'MC-OAuth API',
+      method: 'GET',
+      target: 'https://mc-oauth.andcool.ru/code',
+      tooltip: 'MC-OAuth API',
+      statusPageLink: 'https://mc-oauth.andcool.ru/code',
+      expectedCodes: [200, 404],
+      timeout: 10000,
       headers: {
         'User-Agent': 'Uptimeflare'
       },
