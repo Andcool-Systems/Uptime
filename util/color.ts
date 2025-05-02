4b73a4874c021cc8ad3a40db2182c7370082d27e
AndcoolSystems <drolll2007@gmail.com>
2025-05-02 20:31:03 +0300

    Update color.ts

--- util/color.ts
+++ util/color.ts
@@ -1,33 +1,16 @@
-function getColorText(percent: number | string): string {
-	percent = Number(percent)
-	if (percent >= 99.9) {
-		return '#059669'
-	} else if (percent >= 99) {
-		return '#3bd671'
-	} else if (percent >= 95) {
-		return '#f29030'
-	} else if (Number.isNaN(percent)) {
-		return 'gray'
-	} else {
-		return '#df484a'
-	}
-}
-
 function getColor(percent: number | string, darker: boolean): string {
-	if (darker) return getColorText(percent);
-
-	if (Number.isNaN(Number(percent))) return 'gray';
-
-	const step = 0.5;
-
-	percent = Math.round(Number(percent) / step) * step;
-	percent = Math.max(98, Math.min(percent, 100));
-	const l = map(percent, 98, 100, 55, 65);
-	return `hsl(${map(percent, 98, 100, 0, 120)}deg, ${95}%, ${l}%)`;
+  percent = Number(percent)
+  if (percent >= 99.9) {
+    return darker ? '#059669' : '#3bd671'
+  } else if (percent >= 99) {
+    return darker ? '#3bd671' : '#9deab8'
+  } else if (percent >= 95) {
+    return '#f29030'
+  } else if (Number.isNaN(percent)) {
+    return 'gray'
+  } else {
+    return '#df484a'
+  }
 }
-
-const map = (value: number, inMin: number, inMax: number, outMin: number, outMax: number): number =>
-	((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-
 
 export { getColor }
